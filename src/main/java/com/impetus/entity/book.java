95c9d4790738c9245f35c451450aa0d65801e5d0
God-father1 <balaharode@gmail.com>
2022-02-28 15:20:19 +0530

    book entity

--- src/main/java/com/impetus/entity/book.java
+++ src/main/java/com/impetus/entity/book.java
@@ -1,5 +1,54 @@
 package com.impetus.entity;
 
-public class book {
+import javax.persistence.Entity;
+import javax.persistence.Id;
 
+@Entity
+public class book {
+	@Id
+	private Integer bookId;
+	private String bookName;
+	private String authorName;
+	private Integer quantity;
+	private String subject;
+	public Integer getBookId() {
+		return bookId;
+	}
+	public void setBookId(Integer bookId) {
+		this.bookId = bookId;
+	}
+	public String getBookName() {
+		return bookName;
+	}
+	public void setBookName(String bookName) {
+		this.bookName = bookName;
+	}
+	public String getAuthorName() {
+		return authorName;
+	}
+	public void setAuthorName(String authorName) {
+		this.authorName = authorName;
+	}
+	public Integer getQuantity() {
+		return quantity;
+	}
+	public void setQuantity(Integer quantity) {
+		this.quantity = quantity;
+	}
+	public String getSubject() {
+		return subject;
+	}
+	public void setSubject(String subject) {
+		this.subject = subject;
+	}
+	public book(Integer bookId, String bookName, String authorName, Integer quantity, String subject) {
+		super();
+		this.bookId = bookId;
+		this.bookName = bookName;
+		this.authorName = authorName;
+		this.quantity = quantity;
+		this.subject = subject;
+	}
+	
+	
 }
