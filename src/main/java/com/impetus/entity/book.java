e7ac67883b63efe14df83bae43061d19ed83e11e
God-father1 <balaharode@gmail.com>
2022-02-28 15:19:27 +0530

    initial structure with book entity

---
+++ src/main/java/com/impetus/entity/book.java
@@ -0,0 +1,5 @@
+package com.impetus.entity;
+
+public class book {
+
+}
---
+++ src/test/java/com/impetus/libDemo/LibDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.impetus.libDemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LibDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
